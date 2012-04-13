0fc4c84fbfb18ac48d98ef2d1a7f899e5b4c01b5
Raymond Barbiero <raymond.barbiero.dev@gmail.com>
2012-04-13 23:54:30 +0000

    added quick and dirty install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+DIR=$(cd `dirname $0` ; pwd)
+
+function fullpath() {
+  local PATH="$(cd `dirname $1` ; pwd)/$(basename $1)"
+  echo $PATH
+}
+
+function softlink() {
+  SOURCE=$1
+  DEST=$2
+  if [ -h $DEST ] ; then
+    rm -rf $DEST
+  fi
+  ln -s $SOURCE $DEST
+}
+
+softlink "$DIR/bash_aliases" "$(fullpath ~/.bash_aliases)"
+softlink "$DIR/gitconfig" "$(fullpath ~/.gitconfig)"
+softlink "$DIR/vim" "$(fullpath ~/.vim)"
+softlink "$DIR/vim/vimrc" "$(fullpath ~/.vimrc)"
